ac25dc422c479b368038cff448a78dfc03156d0e
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-20 16:53:32 -0400

    Added Component for Login

---
+++ src/Components/Login.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
+function Login({ BASE_URL, handleLoginSuccess }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLoginSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`${BASE_URL}/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            username,
+            password,
+          },
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        handleLoginSuccess(data.data.token);
+      } else {
+        setErrorMessage('Login failed');
+      }
+    } catch (error) {
+      setErrorMessage('An error occurred during login');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form onSubmit={handleLoginSubmit}>
+        <div>
+          <label htmlFor="loginUsername">Username</label>
+          <input
+            type="text"
+            id="loginUsername"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="loginPassword">Password</label>
+          <input
+            type="password"
+            id="loginPassword"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
