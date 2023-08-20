14ac8ca6c87a03c34ba370b6796b6e486001ca7f
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-20 15:43:31 -0400

    Added link to All Posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import './index.css'
-import NavBar from './Components/NavBar'
-import Home from './Components/Home'
-import AllPosts from './Components/AllPosts'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './Components/NavBar'; 
+import Home from './Components/Home';
+import AllPosts from './Components/AllPosts';
 
-export default function App() {
+function App() {
   const COHORT_NAME = '2302-ACC-PT-WEB-PT-A';
-  const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+  const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<AllPosts BASE_URL={BASE_URL} />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<AllPosts BASE_URL={BASE_URL} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-
+export default App;
 
 
 
