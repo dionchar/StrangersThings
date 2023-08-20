711c2c5f7dce19f7d4c136e57d984951688f4851
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-20 15:37:22 -0400

    Created Component to fetch all Posts

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -2,11 +2,32 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
-export default function AllPosts({ BASE_URL }) {
+function AllPosts({ BASE_URL }) {
+  const [posts, setPosts] = useState([]);
 
-    return (
-        console.log("Home")
-)}
+  useEffect(() => {
+    fetch(`${BASE_URL}/posts`)
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data.data.posts);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }, [BASE_URL]);
+
+  return (
+    <div>
+      <h2>All Posts</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post._id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AllPosts;
