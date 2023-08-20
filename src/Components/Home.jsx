927f7b042164ce3379ff0849d05362e158a33c6d
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-20 15:41:45 -0400

    Created Component to serve as Home Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,24 +7,11 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     
     return (
-        <div>
-        
-          <p className= "welcome-paragraph">
-          Welcome to the Puppy Bowl 2023, the cutest sporting event of the year! 
-          Get ready for a paw-some display of puppy athleticism, where fur and fluff 
-          collide in an epic battle for the title of the Most Adorable Athlete.
-          The halftime show promises to be an absolute treat, featuring the world-famous 
-          Puppy Marching Band and their adorable rendition of "Who Let the Dogs Out."
-          Puppy Bowl 2023 is an event you won't want to miss, where the stakes are high, 
-          the fur is fluffy, and the "awwws" are in abundance. 
-          Get ready for a barking good time!
-          </p>
           <div>
           <p>
             Hello World
           </p>
           <Link to="/posts"> </Link> 
-          </div>
           </div>
       );
     };
