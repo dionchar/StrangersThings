d5b9bed2716a0e3041d8ebc8ca5e47537ff19268
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-22 09:16:12 -0400

    Corrected handleLoginSuccess prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 import NavBar from './Components/NavBar'; 
 import Home from './Components/Home';
@@ -10,26 +12,36 @@ import RegistrationForm from './Components/RegistrationForm';
 import Login from './Components/Login';
 import { fetchWithHeaders } from './Helpers/api';
 
-function App() {
+export default function App() {
   const COHORT_NAME = '2302-ACC-PT-WEB-PT-A';
   const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+  const [token, setToken] = useState('');
+
+  // Define the handleLoginSuccess function
+  const handleLoginSuccess = newToken => {
+    setToken(newToken);
+  };
+
+  const handleLogout = () => {
+    setToken('');
+  };
 
   return (
     <Router>
       <div className="app">
-        <NavBar />
+      <NavBar token={token} handleLogout={handleLogout} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/posts" element={<AllPosts BASE_URL={BASE_URL} />} />
           <Route path="/register" element={<RegistrationForm BASE_URL={BASE_URL} />} />
-          <Route path="/login" element={<Login BASE_URL={BASE_URL} />} />
+          <Route path="/login" element={<Login BASE_URL={BASE_URL} handleLoginSuccess={handleLoginSuccess} />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-export default App;
+
 
 
 
