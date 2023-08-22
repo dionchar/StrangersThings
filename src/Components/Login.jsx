7a89a9cb3d82635cc4200492ccf61a9f1a1a02e7
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-22 13:00:34 -0400

    Added decscriptive comments

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,15 +5,20 @@ import { useNavigate } from 'react-router-dom';
 import { fetchWithHeaders } from '../Helpers/api';
 
 function Login({ BASE_URL, handleLoginSuccess }) {
+  // State to store username and password inputs
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  // State to handle error messages
   const [errorMessage, setErrorMessage] = useState('');
+  // Hook to navigate to different routes
   const navigate = useNavigate();
 
+  // Function to handle login form submission
   const handleLoginSubmit = async event => {
     event.preventDefault();
 
     try {
+      // Make a login request with provided credentials
       const data = await fetchWithHeaders(`${BASE_URL}/users/login`, 'POST', {
         user: {
           username,
@@ -22,12 +27,16 @@ function Login({ BASE_URL, handleLoginSuccess }) {
       });
 
       if (data.success) {
+        // Call the handleLoginSuccess function to set the token
         handleLoginSuccess(data.data.token);
+        // Navigate to the posts page
         navigate('/posts');
       } else {
+        // Display error message for incorrect credentials
         setErrorMessage('Incorrect username or password');
       }
     } catch (error) {
+      // Display error message for any other login errors
       setErrorMessage('An error occurred during login');
     }
   };
