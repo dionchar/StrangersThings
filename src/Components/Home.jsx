4b4f3581afa5f97e2788e4da59f336946a99c563
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-22 13:16:47 -0400

    Added Home Page content

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,18 +2,33 @@
 /* eslint-disable no-unused-vars */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Home = () => {
-    
-    return (
-          <div>
-          <p>
-            Hello World
-          </p>
-          <Link to="/posts"> </Link> 
-          </div>
-      );
-    };
-    
-    export default Home;
+function Home() {
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to Strangers' Things</h1>
+        <p>Your go-to platform for buying and selling unique items!</p>
+      </header>
+      <section className="home-features">
+        <div className="feature">
+          <h2>Explore</h2>
+          <p>Discover a wide range of items from different sellers.</p>
+        </div>
+        <div className="feature">
+          <h2>Sell</h2>
+          <p>List your own items for sale and connect with potential buyers.</p>
+        </div>
+        <div className="feature">
+          <h2>Connect</h2>
+          <p>Communicate with buyers and sellers through our messaging system.</p>
+        </div>
+      </section>
+      <footer className="home-footer">
+        <p>&copy; 2023 Strangers' Things. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
