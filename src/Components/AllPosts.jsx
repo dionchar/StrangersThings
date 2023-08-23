4eaa8cb29a8119607112f0f2e9e09398ad974834
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-22 23:27:10 -0400

    Added functionality to send messages

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -3,19 +3,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import { deletePost } from "../Helpers/api";
+import { deletePost, postMessage } from "../Helpers/api"; // Assuming you have a postMessage function in your Helpers/api.js
 import CreatePostForm from "./CreatePostForm";
 
 function AllPosts({ BASE_URL, token }) {
   // State to store fetched posts
   const [posts, setPosts] = useState([]);
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
+  const [messageInput, setMessageInput] = useState("");
   // State to handle error messages
   const [errorMessage, setErrorMessage] = useState("");
 
   // Fetch posts when the component mounts
   useEffect(() => {
-    fetchUser()
+    fetchUser();
     fetchPosts();
   }, []);
 
@@ -56,6 +57,27 @@ function AllPosts({ BASE_URL, token }) {
 setPosts(posts => posts.filter(post => post._id !== postId)) 
   };
 
+  
+  const handleMessageSubmit = async (event, postId) => {
+    event.preventDefault();
+
+    try {
+      const response = await postMessage(token, postId, messageInput);
+
+      if (response.success) {
+        // Clear the message input and fetch updated posts
+        setMessageInput("");
+        fetchPosts();
+      } else {
+        setErrorMessage("Failed to send message");
+      }
+    } catch (error) {
+      setErrorMessage("An error occurred while sending the message");
+    }
+  };
+
+  
+
   return (
     <div>
       <h2>All Posts</h2>
@@ -64,13 +86,24 @@ setPosts(posts => posts.filter(post => post._id !== postId))
           <div key={post._id}>
             <h3 className="post-title">{post.title}</h3>
             <p className="post-description">{post.description}</p>
-            <p className="post-price">Price: {post.price}</p> 
+            <p className="post-price">Price: {post.price}</p>
             <p className="post-location">Location: {post.location}</p>
             {post?.author?._id === user?._id && (
               <>
                 <p>You are the author of this post.</p>
                 <button onClick={() => handleDeletePost(post._id)}>Delete Post</button>
               </>
+            )}
+            {token && user._id !== post.author._id && (
+              <form onSubmit={(event) => handleMessageSubmit(event, post._id)}>
+                <input
+                  type="text"
+                  value={messageInput}
+                  onChange={(e) => setMessageInput(e.target.value)}
+                  placeholder="Enter your message"
+                />
+                <button type="submit">Send Message</button>
+              </form>
             )}
           </div>
         ))}
