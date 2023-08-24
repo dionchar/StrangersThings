a8e1592fde8b11cbf82ac7a85021dc99bf720056
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-24 11:50:17 -0400

    Code and Comment Clean-Up

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -3,7 +3,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import { fetchWithHeaders, deletePost, postMessage } from "../Helpers/api"; 
+import { fetchWithHeaders, deletePost, postMessage } from "../Helpers/api";
 import CreatePostForm from "./CreatePostForm";
 
 function AllPosts({ BASE_URL, token }) {
@@ -11,7 +11,6 @@ function AllPosts({ BASE_URL, token }) {
   const [posts, setPosts] = useState([]);
   const [user, setUser] = useState({});
   const [messageInput, setMessageInput] = useState("");
-  // State to handle error messages
   const [errorMessage, setErrorMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [userData, setUserData] = useState(null);
@@ -22,8 +21,6 @@ function AllPosts({ BASE_URL, token }) {
     fetchUser();
     fetchPosts();
   }, []);
-
-  
 
   // Function to fetch posts from the API
   const fetchPosts = async () => {
@@ -37,45 +34,36 @@ function AllPosts({ BASE_URL, token }) {
       });
   };
 
+  // function to fetch users data
   const fetchUser = async () => {
-
     try {
       const response = await fetch(`${BASE_URL}/users/me`, {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
       });
       const result = await response.json();
       console.log(result);
-
-      setUser (result.data)
-
+      setUser(result.data);
       setMessages(result.data.messages);
-
     } catch (err) {
       console.error(err);
     }
-  }
-
-  const handleDeletePost = async (postId) => {
-   
-      const response = await deletePost(token, postId);
-
-setPosts(posts => posts.filter(post => post._id !== postId)) 
   };
 
-  
+  // Function to handle deletion of a post
+  const handleDeletePost = async (postId) => {
+    const response = await deletePost(token, postId);
+    setPosts((posts) => posts.filter((post) => post._id !== postId));
+  };
   const handleMessageSubmit = async (event, postId) => {
     event.preventDefault();
-
     try {
       const response = await postMessage(token, postId, messageInput);
-
       if (response.success) {
-        // Clear the message input and fetch updated posts
-        setMessageInput("");
-        fetchPosts();
+        setMessageInput(""); // Clear the message input 
+        fetchPosts(); // fetch updated posts
       } else {
         setErrorMessage("Failed to send message");
       }
@@ -84,19 +72,21 @@ setPosts(posts => posts.filter(post => post._id !== postId))
     }
   };
 
+  // Function for matching text in searh bar
   const postMatches = (post, text) => {
-    // Return true if any of the fields you want to check against include the text
-    // For example, check if title or description includes the text
-    return post.title.toLowerCase().includes(text) || post.description.toLowerCase().includes(text);
+    return (
+      post.title.toLowerCase().includes(text) ||
+      post.description.toLowerCase().includes(text)
+    );
   };
-
-  const filteredPosts = posts.filter((post) => postMatches(post, searchTerm.toLowerCase()));
+  const filteredPosts = posts.filter((post) =>
+    postMatches(post, searchTerm.toLowerCase())
+  );
   const postsToDisplay = searchTerm.length ? filteredPosts : posts;
 
   return (
     <div>
       <header className="allPost-header">
-      
         <div className="allPost-logo">
           <img src="allPost-logo.png" alt="allPost Logo" />
         </div>
@@ -109,13 +99,10 @@ setPosts(posts => posts.filter(post => post._id !== postId))
           />
           <button className="search-button">Search</button>
         </div>
-        
-       
       </header>
       <h2>All Posts</h2>
       {/* ... */}
       <ul className="post-container">
-        
         {postsToDisplay.map((post) => (
           <div key={post._id} className="post-card">
             <h3 className="post-title">{post.title}</h3>
@@ -125,7 +112,9 @@ setPosts(posts => posts.filter(post => post._id !== postId))
             {post?.author?._id === user?._id && (
               <>
                 <p>You are the author of this post.</p>
-                <button onClick={() => handleDeletePost(post._id)}>Delete Post</button>
+                <button onClick={() => handleDeletePost(post._id)}>
+                  Delete Post
+                </button>
               </>
             )}
             {token && user._id !== post.author._id && (
@@ -142,7 +131,11 @@ setPosts(posts => posts.filter(post => post._id !== postId))
           </div>
         ))}
       </ul>
-      <CreatePostForm BASE_URL={BASE_URL} token={token} fetchPosts={fetchPosts} />
+      <CreatePostForm
+        BASE_URL={BASE_URL}
+        token={token}
+        fetchPosts={fetchPosts}
+      />
       <div>
         <h2>Your Messages</h2>
         <ul>
@@ -157,5 +150,5 @@ setPosts(posts => posts.filter(post => post._id !== postId))
       </div>
     </div>
   );
-          }
+}
 export default AllPosts;
