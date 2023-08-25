a6fc0bf57deadd62f36a98d57b2fb291a4bc799c
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-25 11:02:57 -0400

    Corrected Logout Token issue

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -2,6 +2,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { fetchWithHeaders, deletePost, postMessage } from "../Helpers/api";
 import CreatePostForm from "./CreatePostForm";
@@ -17,7 +21,7 @@ function AllPosts({ BASE_URL, token }) {
   const [userData, setUserData] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
 
-  // Fetch posts, users, and set message when the component mounts
+  // Fetch posts when the component mounts
   useEffect(() => {
     fetchUser();
     fetchPosts();
